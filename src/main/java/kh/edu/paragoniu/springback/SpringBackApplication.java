b5f27bae4daa4bbdf10337b86b0735fa4a78e817
atvicheka <at.vicheka@gmail.com>
2026-03-01 14:57:54 +0700

    Website Sunday

---
+++ src/main/java/kh/edu/paragoniu/springback/SpringBackApplication.java
@@ -0,0 +1,14 @@
+package kh.edu.paragoniu.springback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/kh/edu/paragoniu/springback/SpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.paragoniu.springback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
